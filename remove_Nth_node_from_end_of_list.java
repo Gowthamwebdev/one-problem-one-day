f4f3f82a291a36a82e6ac82fe2ccd1a7d3a9b360
Gowtham <137925188+Gowthamwebdev@users.noreply.github.com>
2024-08-07 11:09:28 +0530

    potd '07.08.2024'

---
+++ remove_Nth_node_from_end_of_list.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        
+        ListNode curr = new ListNode(-1);
+        curr.next = head;
+        ListNode fast = curr;
+        ListNode slow = curr;
+        int c = 0;
+
+        while(c < n){
+            c++;
+            fast = fast.next;
+        }
+
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return curr.next;
+    }
+}
